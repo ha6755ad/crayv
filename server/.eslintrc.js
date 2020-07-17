adf5dc73ebbbfbfe99c3dd0a79981757c54d8d27
Bryce Johnson <bryce.johnson@ionrev.com>
2020-07-17 16:27:48 -0600

    fixed eslint issues

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es6: true
+    es6: true,
+    mocha: true
   },
-  'extends': [
-    'eslint:recommended'
+  "extends": [
+    "eslint:recommended"
   ],
   rules: {
-    indent: [2, 2, {SwitchCase: 1}],
+    indent: ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": [
       "error",
       "unix"
@@ -17,11 +18,15 @@ module.exports = {
       "error",
       "single"
     ],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
     ecmaVersion: 2018
   }
 }
