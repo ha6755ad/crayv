7bea9893fcd3001ae4ca7cb96f1b1752ec60932b
Bryce Johnson <bryce.johnson@ionrev.com>
2020-10-09 10:13:02 -0600

    adjustments to baseline and fix for clientside error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,65 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'node': true,
-    'mocha': true
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+  // This option interrupts the configuration hierarchy at this file
+  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'parser': 'babel-eslint'
+
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    mocha: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+
+  // Rules order is important, please avoid shuffling them
+  extends: [
+    // Base ESLint recommended rules
+    'eslint:recommended',
+
+
+    // Uncomment any of the lines below to choose desired strictness,
+    // but leave only one uncommented!
+    // See https://eslint.vuejs.org/rules/#available-rules
+    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
+    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+
+    // https://github.com/prettier/eslint-config-prettier#installation
+    // usage with Prettier, provided by 'eslint-config-prettier'.
+    'prettier',
+
+    'prettier/vue'
   ],
-  'rules': {
-    // "indent": [2, 2, {"VariableDeclarator": "first", "SwitchCase": 1}],
+
+  plugins: [
+    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
+    // required to lint *.vue files
+    'vue',
+
+    // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
+    // Prettier has not been included as plugin to avoid performance impact
+    // add it as an extension for your IDE
+  ],
+
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true,
+    'process': true,
+    'Capacitor': true,
+    'chrome': true
+  },
+
+  // add your custom rules here
+  rules: {
+    'prefer-promise-reject-errors': 'off',
+
     'vue/script-indent': [2, 2, {'baseIndent': 1, 'switchCase': 1, 'ignores': []}],
     'linebreak-style': [
       'error',
@@ -26,17 +72,21 @@ module.exports = {
     'semi': [
       'error',
       'always'
-    ]
+    ],
+
+
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  'overrides': [
+
+  overrides: [
     {
-      'files': [
+      files: [
         '**/__tests__/*.{j,t}s?(x)'
       ],
-      'env': {
-        'mocha': true
+      env: {
+        mocha: true
       }
     }
-  ],
-  'globals': { '_': true },
-};
+  ]
+}
---
+++ client/src/store/services/users.js
@@ -0,0 +1,98 @@
+import feathersClient, {makeServicePlugin, BaseModel} from '../../api/feathers-client';
+
+import {diff} from 'deep-object-diff';
+
+class Users extends BaseModel {
+  constructor(data, options) {
+    super(data, options);
+  }
+
+  // Required for $FeathersVuex plugin to work after production transpile.
+  static modelName = 'Users';
+
+  static diffOnPatch(data) {
+    console.log('diffOnPatch data', data);
+    if (data['_id']) {
+      const originalObject = Users.store.state['users'].keyedById[data['_id']];
+      return diff(originalObject, data);
+    } else {
+      return data;
+    }
+  }
+
+  // Define default properties here
+  static instanceDefaults() {
+    return {
+      // name: null,
+      email: null,
+      // auth0Id: null,
+      // googleId: null,
+      // roles: [],
+      // createdBy: null,
+      // updatedBy: null,
+      // updates: [],
+      theme: {
+        '--q-color-primary': undefined,
+        '--q-color-secondary': undefined,
+        '--q-color-accent': undefined,
+        '--q-color-dark': undefined,
+
+        '--q-color-positive': undefined,
+        '--q-color-negative': undefined,
+        '--q-color-info': undefined,
+        '--q-color-warning': undefined,
+      },
+    };
+  }
+}
+
+const servicePath = 'users';
+const servicePlugin = makeServicePlugin({
+  Model: Users,
+  service: feathersClient.service(servicePath),
+  servicePath,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {}
+});
+
+// const beforeHook = context => {
+//   // eslint-disable-next-line no-console
+//   console.log('------------->>>> beforeHook - context.method:', context.method);
+//   console.log('------------->>>> beforeHook - context.params:', context.params);
+//   console.log('------------->>>> beforeHook - context.data:', context.data);
+// };
+
+// Setup the client-side Feathers hooks.
+feathersClient.service(servicePath).hooks({
+  before: {
+    all: [/*beforeHook*/],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+});
+
+export default servicePlugin;
