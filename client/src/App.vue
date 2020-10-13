cb4c1026afc2d7cf869e394fb526e3cd298d476a
dallinjohnson <dallin.b.johnson@gmail.com>
2020-10-13 14:24:18 -0600

    Add dynamic styling for user.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,39 @@
 <template>
   <div id="q-app">
-    <router-view />
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
+  import {mapGetters} from 'vuex';
+
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {};
+    },
+    watch: {
+      stateUser: {
+        immediate: true,
+        deep: true,
+        handler(newVal) {
+          // console.log('user', newVal);
+          let theme = this.$lget(newVal, 'theme');
+          if (theme) {
+            Object.keys(theme).forEach(k => {
+              if (theme[k]) {
+                this.$setCssVar(k, theme[k]);
+              }
+            });
+          }
+        }
+      },
+    },
+    computed: {
+      ...mapGetters('auth', {
+        stateUser: 'user'
+      }),
+    },
+    methods: {}
   };
 </script>
